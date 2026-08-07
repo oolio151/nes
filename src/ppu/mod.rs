0bed13191c305d5e92e93731d84a4894b7bb7769
Varun Sammeta <diamondblazer59@gmail.com>
2026-08-06 20:20:16 -0400

    starting the ppu wish me luck

--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -1,26 +1,40 @@
 use std::cell::Cell;
 
+// ntsc because pal is for chuds
+pub enum PPURegister {
+    PPUCTRL, // W
+    PPUMASK, // W
+    PPUSTATUS, // R
+    OAMADDR, // W
+    OAMDATA, // RW
+    PPUSCROLL, // Wx2
+    PPUADDR, // Wx2
+    PPUDATA, // RW
+    OAMDMA, // W
+}
+
+
 pub struct PPU {
-    status: Cell<u8>,
-    // ts is just all temporary
+    registers: [u8; 8],
+    scanline: i16, // -1 indicates prerender, goes up to 260
+    dot: u16, // 0 through 340
+    ood_frame: bool, // weird behavior shit
+
 }
 
 impl PPU {
     pub fn new() -> Self {
-        PPU { status: Cell::new(0) }
+        
     }
 
     pub fn read_register(&self, reg: u16) -> u8 {
-        match reg {
-            0x0002 => {
-                let value = self.status.get();
-                self.status.set(value & 0b0111_1111);
-                value
-            }
-            _ => 0,
-        }
+
     }
 
     pub fn write_register(&mut self, reg: u16, data: u8) {
+    }
+
+    pub fn tick(&mut self) {
+
     }
 }
